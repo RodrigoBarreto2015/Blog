38e9043d9455b49eb3fabc773cbd36dfa405376f
RodrigoBarreto2015 <rodrigobarreto2012@hotmail.com>
2023-12-19 08:30:02 -0300

    gitgnore added

--- src/main/java/com/project/blog/model/PostRecord.java
+++
@@ -1,4 +0,0 @@
-package com.project.blog.model;
-
-public record PostRecord(Long id, String title, String description, Boolean published) {
-}
--- src/main/java/com/project/blog/repository/PostRepository.java
+++ src/main/java/com/project/blog/repository/PostRepository.java
@@ -3,13 +3,12 @@ package com.project.blog.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.blog.model.Post;
-import com.project.blog.model.PostRecord;
 
 import java.util.List;
 
 
 public interface PostRepository extends JpaRepository<Post, Long>{
 
-    List <PostRecord> findByPublished(boolean published);
-    List <PostRecord> findByTitleContaining(String title);
+    List <Post> findByPublished(boolean published);
+    List <Post> findByTitleContaining(String title);
 }
